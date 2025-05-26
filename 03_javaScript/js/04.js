e5677f6e720df4f29579f6aa08306e572578e2ad
MingyuAhn <mingool070@naver.com>
2025-05-26 16:02:04 +0900

    커밋

---
+++ 03_javaScript/js/04.js
@@ -0,0 +1,7 @@
+ // 함수를 정의 
+        function over(obj){      //over 함수 : 매개변수로 obj
+            obj.src = './img/ch07/banana.png'; 
+        }
+        function out(obj) {      //out 함수 
+            obj.src='./img/ch07/apple.png'; 
+        }
---
+++ 03_javaScript/js/05.js
@@ -0,0 +1,16 @@
+let name='홍길동';
+let age=23;
+let bol=true;
+let weight=72.55;
+
+// body 에 내용을 출력
+document.write('당신의 <strong>이름</strong>은 : ' + name + '<br><br>');
+document.write('당신의 나이는 : ' + age + '<br><br>');
+document.write('당신은 남자 : ' + bol + '<br><br>');
+document.write('당신의 몸무게는 : ' + weight + '<br><br>');
+
+// console.log 에 출력
+console.log('당신의 <strong>이름</strong>은 : ' + name + '<br><br>');
+console.log('당신의 나이는 : ' + age + '<br><br>');
+console.log('당신은 남자 : ' + bol + '<br><br>');
+console.log('당신의 몸무게는 : ' + weight + '<br><br>');
